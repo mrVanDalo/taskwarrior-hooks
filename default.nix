99c01a06fa2d68e1fc2ab0caec479c0abf798191
Ingolf Wagner <contact@ingolf-wagner.de>
2025-04-04 21:02:12 +0200

    :bug: fix nix 25.05 buildRustPackage deprecation warning
    https://nixos.org/manual/nixpkgs/unstable/release-notes#sec-nixpkgs-release-25.05

--- default.nix
+++ default.nix
@@ -1,16 +1,28 @@
-{ rustPlatform, fetchFromGitHub, stdenv, lib, ... }:
+{
+  rustPlatform,
+  fetchFromGitHub,
+  lib,
+  ...
+}:
 
 rustPlatform.buildRustPackage rec {
+
   name = "taskwarrior-hooks-${version}";
+
   version = "0.2.3";
+
   src = ./.;
-  cargoSha256 = "sha256-O3ui3TmkqLhYorL7zsOLawfWpIZOXb2CBG4ei8OBRGY=";
-  verifyCargoDeps = true;
+
+  useFetchCargoVendor = true;
+
+  #cargoHash = lib.fakeHash;
+  cargoHash = "sha256-+RBwR7O1V8oQgvNVMIGcXeADtGEDZN154NjiY4I40KE=";
+
   meta = with lib; {
-    description = "taskwarrior hook collecton";
+    description = "A fast line-oriented regex search tool, similar to ag and ack";
     homepage = "https://github.com/mrvandalo/taskwarrior-hooks";
-    license = licenses.gpl3Plus;
+    license = licenses.gpl3;
+    maintainers = [ maintainers.mrVanDalo ];
     platforms = platforms.all;
   };
 }
-
